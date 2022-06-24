30fc36626d343ba7f83c2e5fd2ab211817cc7e45
wisdomstar94 <wisdomstar94@gmail.com>
2022-06-24 17:57:27 +0900

    d3.areaRadiul 그려지는 과정 이해 완료.

--- src/test/d3-test-page/d3-011-areaRadial/d3-areaRadial.ts
+++ src/test/d3-test-page/d3-011-areaRadial/d3-areaRadial.ts
@@ -2,10 +2,14 @@ import * as d3 from 'd3';
 
 // https://www.geeksforgeeks.org/d3-js-arearadial-method/
 // https://runebook.dev/ko/docs/d3/d3-shape
+// http://ko.wordow.com/english/dictionary/angle
+// https://runebook.dev/en/docs/d3/d3-shape?page=4#areas
 
-interface Point {
-  x: number;
-  y: number;
+interface Data {
+  startAngle: number;
+  endAngle: number;
+  innerRadius: number;
+  outerRadius: number;
 }
 
 window.addEventListener('load', function() {
@@ -13,30 +17,95 @@ window.addEventListener('load', function() {
 });
 
 function example1() {
-  const points: Point[] = [
-    { x: 10, y: 100 },
-    { x: 90, y: 100 },
-    { x: 100, y: 100 },
-    
-    // { x: 20, y: 0 },
-    // { x: 30, y: 0 },
-    // { x: 40, y: 0 },
-    // { x: 50, y: 0 },
+  const datas: Data[] = [
+    {
+      startAngle: 0,
+      endAngle: 0,
+      innerRadius: 20,
+      outerRadius: 50,
+    },
+    {
+      startAngle: 1.04,
+      endAngle: 1.04,
+      innerRadius: 20,
+      outerRadius: 50,
+    },
+    {
+      startAngle: 2.08,
+      endAngle: 2.08,
+      innerRadius: 20,
+      outerRadius: 50,
+    },
+    {
+      startAngle: 3.12,
+      endAngle: 3.12,
+      innerRadius: 20,
+      outerRadius: 50,
+    },
+    {
+      startAngle: 4.16,
+      endAngle: 4.16,
+      innerRadius: 20,
+      outerRadius: 50,
+    },
+    {
+      startAngle: 5.20,
+      endAngle: 5.20,
+      innerRadius: 20,
+      outerRadius: 50,
+    },
+    {
+      startAngle: 6.28,
+      endAngle: 6.28,
+      innerRadius: 20,
+      outerRadius: 50,
+    },
   ];
 
-  const gen = d3.areaRadial<Point>()
-    .angle(d => d.x)
-    .startAngle(d => d.x + 10)
-    .endAngle(d => d.y)
-    .innerRadius(d => d.y / 3)
-    .outerRadius(d => d.y)
+  /*
+    [ 그려지는 과정 ] 
+    1) 0,0 에서 y 축 위 방향으로 innerRadius 숫자 거리만큼 위치한 곳에 시작점을 찍음.
+    2) 0,0 과 시작점을 잇는 선을 startAngle 각도 수치 만큼 시계 방향으로 회전 시킴.
+    3) 0,0 에서 y 축 위 방향으로 outerRadius 숫자 거리만큼 위치한 곳에 종료점을 찍음.
+    4) 0,0 과 종료점을 잇는 선을 endAngle 각도 수치 만큼 시계 방향으로 회전 시킴.
+    5) 회전 시킨 후 위치한 시작점과 종료점을 이어 선을 그름.
+    6) 마찬가지로 1번~5번 과정을 거쳐 생성된 선과 이전에 생성된 선을 이어 하나의 면적을 만듬.
+  */
+  const gen = d3.areaRadial<Data>()
+    .startAngle(d => {
+      return d.startAngle; 
+    })
+    .endAngle(d => {
+      return d.endAngle;
+    })
+    .innerRadius(d => {
+      return d.innerRadius; 
+    })
+    .outerRadius(d => {
+      return d.outerRadius; 
+    })
   ;
 
   d3.select("svg")
     .append("g")
-    .attr("transform", "translate(100, 100)")
+    .attr("transform", "translate(150, 150)")
     .append("path")
-    .attr("d", gen(points))
+    .attr("d", gen(datas))
     .attr("fill", "green")
     .attr("stroke", "black");
+
+  d3.select("svg").append("g").attr("transform", "translate(150, 150)").append("path")
+    .attr("class", "arc")
+    .attr("d", function(d) {
+      return d3.arc()({
+        innerRadius: 2,
+        outerRadius: 4,
+        startAngle: 0, // 호의 시작 각도, 12시 방향에서 0
+        endAngle: 8, // 호의 끝 각도, 12시 방향에서 0
+        // 각도 증가폭이 커서 소숫점 단위로 증가시켜야 그려지는 각도가 조금씩만 증가함.
+
+        // padAngle: 30,
+      });
+    })
+    .attr("fill","green");
 }
