aca9a45b939c7b5d206adbe06373b5221bd2ae80
wisdomstar94 <wisdomstar94@gmail.com>
2022-07-05 08:32:53 +0900

    d3-030-chord 코드 수정.

--- src/test/d3-test-page/d3-030-chord/d3-chord.ts
+++ src/test/d3-test-page/d3-030-chord/d3-chord.ts
@@ -4,6 +4,7 @@ import * as d3 from 'd3';
 // https://observablehq.com/@d3/directed-chord-diagram
 // https://www.geeksforgeeks.org/d3-js-chord-function/
 // https://www.intothevoid.io/data-visualization/understanding-d3-data-vs-datum/
+// https://www.geeksforgeeks.org/d3-js-ribbon-function/
 
 window.addEventListener('load', async() => {
   var data = [
@@ -38,7 +39,13 @@ window.addEventListener('load', async() => {
     [ d3.data 이란? ]
     - 선택 항목의 각 요소에 대한 바인딩된 데이터를 가져와 반환되는 배열로 결합합니다.
     - 결론을 도출할 수 있는 사실의 모음 (데이터의 복수형)
+
+    [ d3.chord 이란? ]
+    - 데이터를 기반으로 원형 레이아웃을 그리도록 좌표 값 정보를 반환해주는 역할을 합니다.
+    - 주로 d3.arc 와 d3.ribbon 과 같이 사용됩니다.
   */
+
+  // 바깥에 arc 를 그리는 코드
   svg
     .datum(chord) // svg 밑의 g 요소(첫번째 요소)에 chord 값(배열)이 바인딩 됨.
     .append("g") // g 요소 밑에 g 요소를 추가하고 해당 g 요소를 선택함.
@@ -47,15 +54,22 @@ window.addEventListener('load', async() => {
       console.log('one.data.d', d);
       console.log('one.data.d.groups', d.groups);
       return d.groups; 
+      /*
+        [
+          { endAngle: 0.82, index: 0, startAngle: 0, value: 100 },
+          ...
+        ]
+      */
     })
     .enter() // 위 data 에서 받아들인 배열데이터 중에 바인딩 되지 못한 아이템만큼의 가상 요소들을 선택한다. (3개의 가상의 g 요소)
     .append("g") // 위 enter 에서 가상요소를 선택한 부분에 실제 g 요소를 추가한다. (이 때 총 4개의 g 요소가 선택된 상태)
     .append("path") // 4개의 g 요소 각각의 밑에 path 요소를 추가한다.
     .style("fill", "black") // 4개의 g 요소 각각의 밑에 추가된 path 들에 style 을 지정한다.
     .style("stroke", "black") // 4개의 g 요소 각각의 밑에 추가된 path 들에 style 을 지정한다.
-    .attr("d", d3.arc().innerRadius(140).outerRadius(160) as any) // // 4개의 g 요소 각각의 밑에 추가된 path 들에 attr 을 지정한다.
+    .attr("d", d3.arc().innerRadius(140).outerRadius(160) as any) // 4개의 g 요소 각각의 밑에 추가된 path 들에 attr 을 지정한다.
   ;
 
+  // arc 를 기준으로 ribbon 을 그리는 코드
   svg
     .datum(chord) // svg 요소(첫번째 요소)에 chord 값(배열)이 바인딩 됨.
     .append("g") // svg 요소 밑에 g 요소를 추가한다.
@@ -63,6 +77,15 @@ window.addEventListener('load', async() => {
     .data(function (d) { // data 에서 datum 에서 바인딩된 chord 값(배열)을 d 라는 변수로 받는 함수를 호출하여 반환되는 값(d)을 데이터로써 받아들인다.
       console.log('two.data.d', d);
       return d; 
+      /*
+        [
+          {
+            source: { endAngle: 0.08, index: 0, startAngle: 0, value: 10 },
+            target: { endAngle: 0.08, index: 0, startAngle: 0, value: 10 },
+          },
+          ...
+        ]
+      */
     })
     .enter() // 위 data 에서 받아들인 배열데이터 중에 바인딩 되지 못한 아이템만큼의 가상 요소들을 선택한다. (10개의 가상의 path 요소)
     .append("path") // 위 enter 에서 가상요소를 선택한 부분에 실제 path 요소를 추가한다. (이 때 총 10개의 path 요소가 선택된 상태)
