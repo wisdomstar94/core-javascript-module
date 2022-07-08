882d555c1e5d545f6a135db219ecc74a37c04904
wisdomstar94 <wisdomstar94@gmail.com>
2022-07-09 00:09:01 +0900

    d3-active 코드 수정.

--- src/test/d3-test-page/d3-008-active/d3-active.ts
+++ src/test/d3-test-page/d3-008-active/d3-active.ts
@@ -1,6 +1,8 @@
 import * as d3 from 'd3';
 
 // https://github.com/d3/d3-transition#active
+// https://www.geeksforgeeks.org/d3-js-interpolatergb-function/
+// https://runebook.dev/en/docs/d3/d3-transition?page=2#transition_tween
 
 window.addEventListener('load', function() {
   const n = 10;
@@ -9,31 +11,43 @@ window.addEventListener('load', function() {
   const blueorange = d3.interpolateRgb("steelblue", "orange");
   const orangewhite = d3.interpolateRgb("orange", "#eee");
 
-  d3.select("body").selectAll("div")
+  d3
+    .select("body")
+    .selectAll("div")
     .data(d3.range(n))
     .enter()
     .append("div")
     .transition()
-      .delay(function(d, i) { 
-        const delayValue = i + Math.random() * n / 4; 
-        console.log('delayValue', delayValue);
-        return delayValue; 
-      })
-      .ease(d3.easeLinear)
-      .on("start", function repeat() {
-        console.log('repeat');
+    .delay(function(d, i) { 
+      const delayValue = (i + Math.random() * n / 4) * 1000; 
+      console.log('delayValue', delayValue);
+      return delayValue; 
+    })
+    .ease(d3.easeLinear)
+    .on("start", function repeat() {
+      console.log('repeat', d3.active(this));
 
-        d3.active(this)
-            ?.styleTween("background-color", function() { return whiteblue; })
-          .transition()
-            .delay(1000)
-            .styleTween("background-color", function() { return blueorange; })
-          .transition()
-            .delay(1000)
-            .styleTween("background-color", function() { return orangewhite; })
-          .transition()
-            .delay(n)
-            .on("start", repeat);
+      d3
+        .active(this)
+        ?.styleTween("background-color", function() { 
+          console.log('styleTween : 회색 -> 파랑색'); // 회색 -> 파랑색
+          return whiteblue; 
+        })
+        .transition()
+        .delay(1000) // 1초 딜레이 적용
+        .styleTween("background-color", function() { 
+          console.log('styleTween : 파랑색 -> 주황색'); // 파랑색 -> 주황색
+          return blueorange; 
+        })
+        .transition()
+        .delay(1000) // 1초 딜레이 적용
+        .styleTween("background-color", function() { 
+          console.log('styleTween : 주황색 -> 회색'); // 주황색 -> 회색
+          return orangewhite; 
+        })
+        .transition()
+        .delay(1000) // 1초 딜레이 적용
+        .on("start", repeat);
       });
 });
 
