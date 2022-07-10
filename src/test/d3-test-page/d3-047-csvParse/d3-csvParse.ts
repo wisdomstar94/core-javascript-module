14503e247533e1ddecd36f4470ff97a0b0777f12
wisdomstar94 <wisdomstar94@gmail.com>
2022-07-10 14:35:34 +0900

    d3-047-csvParse 코드 작성.

---
+++ src/test/d3-test-page/d3-047-csvParse/d3-csvParse.ts
@@ -0,0 +1,19 @@
+import * as d3 from 'd3';
+
+window.addEventListener('load', async() => {
+  /*
+    d3.csvParse 함수는 csv 포맷의 데이터를 js 데이터로 변환해주는 함수입니다.
+  */
+  const csvParse = d3.csvParse(`
+name,price
+apple,"70,000"
+melon,"35,300"
+`.trim());
+  console.log('csvParse');
+  console.log(csvParse);
+  /*
+    0: {name: 'apple', price: '70,000'}
+    1: {name: 'melon', price: '35,300'}
+    columns: (2) ['name', 'price']
+  */
+});
