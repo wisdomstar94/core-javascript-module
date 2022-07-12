9015c7a0e87245e57c0a14f5d5ae1a02205410c2
wisdomstar94 <wisdomstar94@gmail.com>
2022-07-12 21:48:21 +0900

    d3-009-arc 코드 수정.

--- src/test/d3-test-page/d3-009-arc/d3-arc.ts
+++ src/test/d3-test-page/d3-009-arc/d3-arc.ts
@@ -11,12 +11,11 @@ window.addEventListener('load', function() {
     .attr("d", function(d) {
       return d3.arc()({
         innerRadius: 40,
-        outerRadius: 60,
-        startAngle: 0, // 호의 시작 각도, 12시 방향에서 0
-        endAngle: 2.2, // 호의 끝 각도, 12시 방향에서 0
+        outerRadius: 70,
+        // outerRadius 와 innerRadius 사이의 영역에 호가 그려집니다. 즉, 호의 굵기를 크게하려면 outerRadius - innerRadius 값이 크게 나오게 하면 됨.
+        startAngle: 0, // 호의 시작 각도, 0 이 12시 방향이고 값이 커질 수록 시계방향으로 이동됨.
+        endAngle: 2.2, // 호의 끝 각도, 0 이 12시 방향이고 값이 커질 수록 시계방향으로 이동됨.
         // 각도 증가폭이 커서 소숫점 단위로 증가시켜야 그려지는 각도가 조금씩만 증가함.
-
-        // padAngle: 30,
       });
     })
     .attr("fill","green")
