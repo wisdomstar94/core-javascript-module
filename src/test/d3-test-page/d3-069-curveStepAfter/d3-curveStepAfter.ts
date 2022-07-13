aa8b2d87324fe4a986c8015c96006021449bf4e6
wisdomstar94 <wisdomstar94@gmail.com>
2022-07-13 21:27:36 +0900

    d3-069-curveStepAfter 코드 작성.

---
+++ src/test/d3-test-page/d3-069-curveStepAfter/d3-curveStepAfter.ts
@@ -0,0 +1,61 @@
+import * as d3 from 'd3';
+
+// https://www.geeksforgeeks.org/d3-js-curvestepafter-method/
+// https://github.com/d3/d3-shape#curveStepAfter
+
+export declare namespace ID3CurveStepAfter {
+  export interface Data {
+    x: number;
+    y: number;
+  }
+}
+
+window.addEventListener('load', async() => {
+  /*
+    d3.curveStepAfter 는 수평선과 수직선으로 구성된 부분별 상수 함수(단계 함수)를 생성합니다. y-값은 x-값 이후에 변경됩니다.
+  */
+
+  const data: ID3CurveStepAfter.Data[] = [
+    {x: 0, y: 20},
+    {x: 10, y: 15},
+    {x: 20, y: 50},
+    {x: 30, y: 35},
+    {x: 40, y: 70},
+    {x: 50, y: 115},
+    {x: 60, y: 89},
+    {x: 90, y: 60},
+    {x: 125, y: 15},
+  ];
+
+  /* Area */
+  const areaGenerator = d3.area<ID3CurveStepAfter.Data>()
+    .x((d) => d.x)
+    .y1((d) => 50)
+    .y0((d) => d.y)
+    .curve(d3.curveStepAfter)
+    ;
+
+  /* Line */
+  const lineGenerator = d3.line<ID3CurveStepAfter.Data>()
+    .x((d) => d.x)
+    .y((d) => d.y)
+    .curve(d3.curveStepAfter)
+  ;
+
+  const svg1 = d3.select('svg.svg-1');
+  const svg2 = d3.select('svg.svg-2');
+
+  svg1
+    .append("path")
+    .attr("d", areaGenerator(data))
+    .attr("fill", "green")
+    .attr("stroke", "black")
+  ;
+
+  svg2
+    .append("path")
+    .attr("d", lineGenerator(data))
+    .attr("fill", "green")
+    .attr("stroke", "black")
+  ;
+});
