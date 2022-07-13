597cfc5bbaa7b85105c001d1682d2bc0d19aacbc
wisdomstar94 <wisdomstar94@gmail.com>
2022-07-13 21:05:58 +0900

    d3-062-curveCatmullRomOpen 코드 작성.

---
+++ src/test/d3-test-page/d3-062-curveCatmullRomOpen/d3-curveCatmullRomOpen.ts
@@ -0,0 +1,62 @@
+import * as d3 from 'd3';
+
+// https://www.geeksforgeeks.org/d3-js-curvecatmullromopen-method/
+// https://github.com/d3/d3-shape#curveCatmullRomOpen
+
+export declare namespace ID3CurveCatmullRomOpen {
+  export interface Data {
+    x: number;
+    y: number;
+  }
+}
+
+window.addEventListener('load', async() => {
+  /*
+    d3.curveCatmullRomOpen 는 지정된 제어점과 매개변수 알파(기본값은 0.5)를 사용하여 3차 Catmull-Rom 스플라인을 생성합니다. 
+    CurveCatmullRom과 달리 첫 번째 조각과 마지막 조각에 단측 차이가 사용되지 않으므로 곡선은 두 번째 점에서 시작하여 끝에서 두 번째 점에서 끝납니다.
+  */
+
+  const data: ID3CurveCatmullRomOpen.Data[] = [
+    {x: 0, y: 20},
+    {x: 10, y: 15},
+    {x: 20, y: 50},
+    {x: 30, y: 35},
+    {x: 40, y: 70},
+    {x: 50, y: 115},
+    {x: 60, y: 89},
+    {x: 90, y: 60},
+    {x: 125, y: 15},
+  ];
+
+  /* Area */
+  const areaGenerator = d3.area<ID3CurveCatmullRomOpen.Data>()
+    .x((d) => d.x)
+    .y1((d) => 50)
+    .y0((d) => d.y)
+    .curve(d3.curveCatmullRomOpen)
+    ;
+
+  /* Line */
+  const lineGenerator = d3.line<ID3CurveCatmullRomOpen.Data>()
+    .x((d) => d.x)
+    .y((d) => d.y)
+    .curve(d3.curveCatmullRomOpen)
+  ;
+
+  const svg1 = d3.select('svg.svg-1');
+  const svg2 = d3.select('svg.svg-2');
+
+  svg1
+    .append("path")
+    .attr("d", areaGenerator(data))
+    .attr("fill", "green")
+    .attr("stroke", "black")
+  ;
+
+  svg2
+    .append("path")
+    .attr("d", lineGenerator(data))
+    .attr("fill", "green")
+    .attr("stroke", "black")
+  ;
+});
