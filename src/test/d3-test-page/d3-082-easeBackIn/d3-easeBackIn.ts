9d6e3c045e27d03a96c2f6e76f2bea75fa261f9b
wisdomstar94 <wisdomstar94@gmail.com>
2022-07-15 21:46:21 +0900

    d3-082-easeBackIn 코드 작성.

---
+++ src/test/d3-test-page/d3-082-easeBackIn/d3-easeBackIn.ts
@@ -0,0 +1,24 @@
+import * as d3 from 'd3';
+
+// https://observablehq.com/@d3/easing-animations
+
+window.addEventListener('load', async() => {  
+  /*
+    d3.easeBack 는 transition 의 ease 종류중 하나입니다.
+  */
+
+  const svg = d3.select('svg');
+  
+  const circle = svg
+    .append('g')
+    .append('circle')
+    .attr('r', 10)
+    .attr('cx', 20)
+    .attr('cy', 20)
+    .attr('style', 'fill: #f00')
+    .transition()
+    .ease(d3.easeBackIn)
+    .duration(1000)
+    .attr('cx', 100)
+  ;
+});
