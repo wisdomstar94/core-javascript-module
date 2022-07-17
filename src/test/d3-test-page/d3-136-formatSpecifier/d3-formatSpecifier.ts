baea458de31a9d4985940623897ae6850dd67353
wisdomstar94 <wisdomstar94@gmail.com>
2022-07-17 14:20:54 +0900

    d3-136-formatSpecifier 코드 작성.

---
+++ src/test/d3-test-page/d3-136-formatSpecifier/d3-formatSpecifier.ts
@@ -0,0 +1,32 @@
+import * as d3 from 'd3';
+
+// https://devdocs.io/d3~7/d3-format#formatspecifier
+
+window.addEventListener('load', async() => {  
+  /*
+    d3.formatSpecifier
+    => 지정된 지정자를 구문 분석하여 형식 사양 미니 언어 및 지정자를 재구성하는 toString 메서드에 해당하는 노출된 필드가 있는 개체를 반환합니다. 
+       예를 들어, formatSpecifier("s")는 다음을 반환합니다.
+
+       FormatSpecifier {
+        "fill": " ",
+        "align": ">",
+        "sign": "-",
+        "symbol": "",
+        "zero": false,
+        "width": undefined,
+        "comma": false,
+        "precision": undefined,
+        "trim": false,
+        "type": "s"
+      }
+
+      이 방법은 형식 지정자를 구문 분석하고 새 지정자를 파생하는 방법을 이해하는 데 유용합니다. 
+      예를 들어, precisionFixed를 사용하여 형식을 지정하려는 숫자를 기반으로 적절한 정밀도를 계산한 다음 새 형식을 만들 수 있습니다.
+  */
+
+  const s = d3.formatSpecifier("f");
+  s.precision = d3.precisionFixed(0.01);
+  const f = d3.format(s.toString());
+  console.log(`f(42)`, f(42)); // "42.00";
+});
