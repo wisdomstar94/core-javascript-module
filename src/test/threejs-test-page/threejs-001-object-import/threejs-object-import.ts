62211aed6817e00744910a2da44150494a6245fb
wisdomstar94 <wisdomstar94@gmail.com>
2022-08-05 07:56:48 +0900

    three-002-webgl-check 코드 작성.

--- src/test/threejs-test-page/threejs-001-object-import/threejs-object-import.ts
+++
@@ -1,5 +0,0 @@
-import * as THREE from 'three';
-
-window.addEventListener('load', () => {
-  
-});
--- src/test/threejs/three-002-webgl-check/three-webgl-check.esbuild.js
+++ src/test/threejs/three-002-webgl-check/three-webgl-check.esbuild.js
@@ -1,11 +1,12 @@
 
 const path = require('path');
-const targetFileName = 'threejs-object-import';
+const targetFileName = 'three-webgl-check';
 require('esbuild').build({
   // the entry point file described above
   entryPoints: [path.join(__dirname, `${targetFileName}.ts`)],
   // the build folder location described above
-  outfile: path.join(__dirname, `${targetFileName}.js`),
+  outfile: path.join(__dirname, `dist`, `${targetFileName}.js`),
+  // outdir: path.join(__dirname, 'dist'),
   platform: 'browser',
   bundle: true,
   target: ['chrome60', 'firefox60', 'safari11', 'edge20'],
